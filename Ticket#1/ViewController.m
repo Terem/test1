cfec6e715796488b79ff23d6a2bc0dd89efbbfa5
Terem <terem45@yandex.ru>
2013-03-30 18:53:46 +0200

    ticket:1 my

--- Ticket#1/ViewController.m
+++ Ticket#1/ViewController.m
@@ -21,39 +21,35 @@
     UIInterfaceOrientation destOrientation = self.interfaceOrientation;
     if (destOrientation == UIInterfaceOrientationPortrait ||
         destOrientation == UIInterfaceOrientationPortraitUpsideDown) {
-        //—-if rotating to portrait mode—-
         myImage.frame = CGRectMake(20, 43, 128, 128);
         lastName.frame = CGRectMake(180,76,100,21);
         firstName.frame = CGRectMake(180,116,100,21);
         surName.frame = CGRectMake(180,156,100,21);
         dateOfBirth.frame = CGRectMake(180,196,100,21);
         bio.frame = CGRectMake(57,244,249,120);
-        contacts.frame = CGRectMake(0,0,223,21);
-        [self.view setNeedsDisplay];
+        contacts.frame = CGRectMake(57,380,223,21);
+        NSLog(@"Portrait!!!!!!!!!!");
     } else {
-        NSLog(@"Landscape!!!!!!!!!!");
-        //—-if rotating to landscape mode—-
         myImage.frame = CGRectMake(20, 43, 128, 128);
-        lastName.frame = CGRectMake(195,64,100,21);
-        firstName.frame = CGRectMake(306,64,100,21);
-        surName.frame = CGRectMake(413,64,100,21);
-        dateOfBirth.frame = CGRectMake(195,117,100,21);
-        bio.frame = CGRectMake(284,216,171,120);
-        contacts.frame = CGRectMake(188,241,100,21);
-        [self.view setNeedsDisplay];
+        lastName.frame = CGRectMake(160,64,100,21);
+        firstName.frame = CGRectMake(260,64,100,21);
+        surName.frame = CGRectMake(360,64,100,21);
+        dateOfBirth.frame = CGRectMake(160,100,100,21);
+        bio.frame = CGRectMake(160,160,300,80);
+        contacts.frame = CGRectMake(160,270,200,21);
+        NSLog(@"Landscape!!!!!!!!!!");
     }
 }
 
-/*- (void)willAnimateSecondHalfOfRotationFromInterfaceOrientation:
-(UIInterfaceOrientation)fromInterfaceOrientation duration:(NSTimeInterval)duration {
+-(void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
     [self positionViews];
-}*/
-
+}
 
 - (void)viewDidLoad
 {
-    [self positionViews];
     [super viewDidLoad];
+    //[self positionViews];
 	// Do any additional setup after loading the view, typically from a nib.
     NSString *path=[[[NSBundle mainBundle]resourcePath]stringByAppendingPathComponent:@"my_db.sqlite"];
     FMDatabase *database;
@@ -62,8 +58,6 @@
     [database open];
     FMResultSet *results=[database executeQuery:@"select * from personal_data"];
     while ([results next]) {
-        //NSString *lastN=[results stringForColumn:@"lastname"];
-        //[self setLastName:[NSString stringWithFormat:@"%@",lastN]];
         lastName.text=[results stringForColumn:@"lastname"];
         firstName.text=[results stringForColumn:@"firstname"];
         surName.text=[results stringForColumn:@"surname"];
@@ -74,27 +68,7 @@
         NSLog(@"FOTO : %@",imageData);
         myImage.image=[UIImage imageWithData:imageData];
     }
-}
-
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
+    [self positionViews];
 }
 
 
@@ -108,6 +82,7 @@
     //return (interfaceOrientation == UIInterfaceOrientationLandscapeLeft);
     
 }
+ 
 
 
 
